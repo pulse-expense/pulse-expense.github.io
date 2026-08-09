d03a16e0855100007495952698e011fa967c8d08
pulse-expense <pulseexpenseapp@gmail.com>
2026-08-09 12:03:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pulse-commercial-v13-financial-health";
+const CACHE="pulse-commercial-v14-final";
 const CORE=["./","./index.html","./manifest.json","./config.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
